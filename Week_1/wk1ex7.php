5dcc5deef4c0e254bf2451b6798c2bc0ccf44de9
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-04 01:17:47 +0000

    updated the ex7

--- Week_1/wk1ex7.php
+++ Week_1/wk1ex7.php
@@ -2,20 +2,20 @@
     <header>
         <title>My Php Lob Book website</title>
     </header>
+    <body>
+        <center>
+            <?php
+                $hourlyrate = 5.75;
+                $hoursperweek = 40;
+                $taxtrate =.22;
+                $gross = $hourlyrate * $hoursperweek;
+                echo $gross, "<br/>";
+                $taxtrate = $gross*$taxtrate;
+                echo $taxtrate, "<br/>";
+                $netwage =$gross-$taxtrate;
+                echo $netwage;
+            ?>
+        </center>
+    </body>
 </html>
-<body>
-    <center>
-        <?php
-            $hourlyrate = 5.75;
-            $hoursperweek = 40;
-            $taxtrate =.22;
-            $gross = $hourlyrate * $hoursperweek;
-            echo $gross, "<br/>";
-            $taxtrate = $gross*$taxtrate;
-            echo $taxtrate, "<br/>";
-            $netwage =$gross-$taxtrate;
-            echo $netwage;
-        ?>
-    </center>
-</body>
 
