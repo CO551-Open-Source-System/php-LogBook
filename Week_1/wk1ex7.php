37857f3fece8606c815b54af2829210f5f9281ce
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-04 01:06:13 +0000

    completed  ex7

---
+++ Week_1/wk1ex7.php
@@ -0,0 +1,21 @@
+<html>
+    <header>
+        <title>My Php Lob Book website</title>
+    </header>
+</html>
+<body>
+    <center>
+        <?php
+            $hourlyrate = 5.75;
+            $hoursperweek = 40;
+            $taxtrate =.22;
+            $gross = $hourlyrate * $hoursperweek;
+            echo $gross, "<br/>";
+            $taxtrate = $gross*$taxtrate;
+            echo $taxtrate, "<br/>";
+            $netwage =$gross-$taxtrate;
+            echo $netwage;
+        ?>
+    </center>
+</body>
+
