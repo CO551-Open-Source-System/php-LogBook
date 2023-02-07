66680efb2b2785ee75648fe36e1827a7dd59dfd6
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-07 22:01:07 +0000

    completed ex9

---
+++ Week_2/wk2ex9.php
@@ -0,0 +1,13 @@
+<?php
+  $topmodules[0] = "Internet Systems Development";
+  $topmodules[5] = "Programming 1";
+  $topmodules[10] = "Programming 2";
+  $topmodules[30] = "OOAD";
+  $topmodules[40] = "Software Engineering";
+
+  for ($count = 0; $count < 30; $count++);
+  {
+    echo "Index is $count and value is $topModules[$count] <br/>";
+  }
+?>
+
