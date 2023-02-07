84d23396fafd87bea3be26955b29db06093ca41c
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-07 21:09:21 +0000

    completed

--- Week_2/wk2ex2.php
+++ Week_2/wk2ex2.php
@@ -1,11 +1,11 @@
 <?php 
-$myage = "None of your business <br/>";
+	$myage = "None of your business <br/>";
 	print($myage);
+	
 	$myage  = 21;
+    print($myage. "<br/>");
     
-	print($myage.$myage)
-    
-	$myage float= 21.75;
+	$myage = 21.75;
 	print($myage);
 ?>	
 
