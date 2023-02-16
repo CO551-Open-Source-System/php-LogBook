5edc32d82faa56643d2159792cf22c312c99e27f
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-16 17:55:44 +0000

    ifelse

---
+++ Week2OtherWorks/ifelseifstatment.php
@@ -0,0 +1,11 @@
+<?php
+        $grade =62;
+        //$pre
+        if($grade >=70){
+        echo 'Distinction';
+        }elseif($grade >=55){
+            echo 'Merit';
+        }elseif($grade >=40){
+            echo 'Pass';
+        }else {echo 'Failed';}
+?>
