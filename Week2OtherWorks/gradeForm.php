0f12a7841b6b278c61cdf4aebf8d0d313c742268
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-16 17:55:26 +0000

    updateed

---
+++ Week2OtherWorks/gradeForm.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h3>My Exercise Week-2</h3>
+    </head>
+        <body>
+            <form method="post" action="grade.php">
+                Please enter your grade:
+                <p><input type="text" name="grade"></p>
+                <input type="submit" value="Submit">
+            </form>            
+        </body>
+</html>
