302d9fbee630f776aaefebd175a67a5ed3b6fa72
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-16 18:52:32 +0000

    added for loop

---
+++ Week2OtherWorks/forloop.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <h2>My Exercise</h2>
+    </head>
+    <body>
+        <?php
+        for ($i=0; $i <=10; $i++) { 
+            echo "$1 if i want to pass i must attend the lectures <br/>";
+        }     
+        ?>
+    </body>
+</html>
