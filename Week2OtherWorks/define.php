bbd8fd2bf34484cad1a7b0d8aaedc612547520b8
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-16 12:46:23 +0000

    wk1 define method

---
+++ Week2OtherWorks/define.php
@@ -0,0 +1,9 @@
+<?php
+$myage = 21;
+//define("RETIREMENTAGE",68);
+define("RetirmentAge",68);
+
+//$yearsleft = RETIREMENTAGE -$myage;
+$yearsleft = RetirmentAge -$myage;
+echo "You have $yearsleft years to go";
+?>
