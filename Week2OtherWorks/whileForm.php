f86dd24788cccb6c9672368a7d78fa840ea3fb4f
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-16 18:41:24 +0000

    added whilefrom

---
+++ Week2OtherWorks/whileForm.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h2>My Exercise</h2>       
+    </head>
+    <body>
+        <form method="post" action="while.php">
+             Balance details :<br/>
+        <input type="submit" value="Print">  
+
+    </body>
+</html>
