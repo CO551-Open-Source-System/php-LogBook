d0663dea6cbf5fa75900ed6f6026c201b489b1d2
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-16 12:58:18 +0000

    updated ifelse

---
+++ Week2OtherWorks/ifelsestatment.php
@@ -0,0 +1,9 @@
+<?php
+    $age =68;
+    if($age >= 30 and $age < 50){
+        echo "You are entitled to a pension";
+    }
+    else{
+        echo "You are either inexperienced or past it";
+    }
+?>
---
+++ Week2OtherWorks/ifstatment.php
@@ -0,0 +1,6 @@
+<?php
+    $age =68;
+    if($age <= 68){
+        echo "You are entitled to a pension";
+    }
+?>
