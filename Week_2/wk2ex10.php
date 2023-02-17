0d3e79ea11e950def005f9a9045be7a3a321cc07
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-17 11:54:49 +0000

    Update wk2ex10.php

--- Week_2/wk2ex10.php
+++ Week_2/wk2ex10.php
@@ -4,9 +4,9 @@
   $mymarks["year 3"] = 75;
 
   // foreach loop here
-  for ($index =0; $index < 75; $index++);
-  {
-    echo "for  $index  my grade was  $mymarks[$index] <br/>";
+  foreach ($mymarks as $index => $value) {
+      
+    echo "for  $index  my grade was $mymarks[$index] <br/>";
   }
   echo "<br/> My best year was Year 3 when I averaged ". $mymarks["year 3"];
 ?>
