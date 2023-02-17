422a2ea11aac315f792edf0b21b29d78e7d9ff11
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-17 13:05:35 +0000

    updated

--- Week_2/wk2ex11.php
+++ Week_2/wk2ex11.php
@@ -1,6 +1,15 @@
 <?php
+$mymarks["year 1"] = 55;
+$mymarks["year 2"] = 65;
+$mymarks["year 3"] = 75;
+
 $total = 0;
-$total = $total + $mymarks[$index];
-$average = $total /  6;
-echo ($total . $average);
+
+$average = $total /  3;
+foreach ($mymarks as $index => $value) {
+    $total = $total + $mymarks[$index];
+    echo "Myaverage mark. $average";
+}
+
+//echo ($total . $average);
 ?>   
