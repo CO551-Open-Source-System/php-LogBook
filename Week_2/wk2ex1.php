1b8c3d4dc2b486d0403da56d02921565d7c2e8d6
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-17 10:38:17 +0000

    updated ex1

--- Week_2/wk2ex1.php
+++ Week_2/wk2ex1.php
@@ -1,13 +1,11 @@
-<html> 
-    <head> 
-        <titl>My gross wage is</titl>
-    </head>
-    <body>
-        <?php
-        $hourlyrate	= 5.75;
-        $hoursperweek = 40;
-        $gross = $hourlyrate * $hoursperweek;
-        print("$gross"); 
-        ?>	
-    </body>
+<?php 
+    $hourlyrate	= 5.75;
+	$hoursperweek = 40;
+	$gross = $hourlyrate * $hoursperweek;
+?>	
+<html>
+<head></head>
+<body>
+	<p> My gross wage is <?php print("$gross"); ?>
+</body>
 </html>
