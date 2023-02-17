1f4c1ef8c6f481bd6e382291ecce87b64a664b68
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-17 11:27:04 +0000

    updated ex8

--- Week_2/wk2ex8.php
+++ Week_2/wk2ex8.php
@@ -1,14 +1,13 @@
 <?php
-  $topModules[0] = "Internet Systems Development";
-  $topModules[1] = "Programming 1";
-  $topModules[2] = "Programming 2";
-  $topModules[3] = "OOAD";
-  $topModules[4] = "Software Engineering";
-  $topModules[5] = "Networking";
-  $topModules[6] = "Open Source System";   
+$topModules[0] = "Internet Systems Development";
+$topModules[1] = "Programming 1";
+$topModules[2] = "Programming 2";
+$topModules[3] = "OOAD";
+$topModules[4] = "Software Engineering";
+$topModules[5] = "Networking";
+$topModules[6] = "Open Source System";   
 
-for ($count = 0; $count < 2; $count++);
-  {
+for ( $count=0; $count < 6; $count++) {
     echo "$count module is .$topModules[$count] <br/>";
-  }	
+    }
 ?>
