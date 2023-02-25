b8ed83a25f68b3eea5894ce405b1a867373c7977
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-25 23:17:14 +0000

    Update selectcolour.php

--- Week_4/Exercise 3/selectcolour.php
+++ Week_4/Exercise 3/selectcolour.php
@@ -1,17 +1,24 @@
+<?php
+session_start();
+$_SESSION['qty'] = $_POST['selqty'];
+$_SESSION['price'] = $_POST['txtprice'];
+?>
+
 <html>
-  <head><title>Select colour page</title></head>
-    <body>
-      <form action="confirmation.php"  method="post">
-	Select the colour for the <?php echo $_POST[selqty] ?> widgets you are ordering
-   	<select name="selcolour">
-   	  <option>white</option>
-	  <option>red</option>
-	  <option>yellow</option>
-	  <option>green</option>
-	  <option>blue</option>
-        </select>
-        <br/><br/>	
-        <input type="submit" value="Buy"/>
-      </form>
-   </body>
-</html>	
+  <head>
+    <title>Select Color Page</title>
+  </head>
+  <body>
+    <form action="confirmation.php" method="post">
+      Select the color for the <?php echo $_SESSION['qty']; ?> widgets you are ordering:<br>
+      <select name="selcolour">
+        <option value="white">White</option>
+        <option value="red">Red</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+      </select><br><br>
+      <input type="submit" value="Buy">
+    </form>
+  </body>
+</html>
