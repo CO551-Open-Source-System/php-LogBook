9781bb66d45c168eebc0242bbe207940f5cf687a
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-25 23:17:06 +0000

    Create selectcoloursession.php

---
+++ Week_4/Exercise 2/selectcoloursession.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$_SESSION['qty'] = $_POST['selqty'];
+$_SESSION['price'] = $_POST['txtprice'];
+?>
+
+<html>
+  <head>
+    <title>Select Color Page</title>
+  </head>
+  <body>
+    <form action="confirmationsession.php" method="post">
+      Select the color for the widgets you are ordering:<br>
+      <select name="selcolour">
+        <option value="white">White</option>
+        <option value="red">Red</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+      </select><br><br>
+      <input type="submit" value="Buy">
+    </form>
+  </body>
+</html>
