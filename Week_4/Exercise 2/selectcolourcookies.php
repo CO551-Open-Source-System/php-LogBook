a6ebade8a267615539173768ed8e87de330198ee
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-25 23:17:26 +0000

    Update selectcolourcookies.php

--- Week_4/Exercise 2/selectcolourcookies.php
+++ Week_4/Exercise 2/selectcolourcookies.php
@@ -1,8 +1,11 @@
 <html>
   <head><title>Select colour page</title></head>
     <body>
-      <form action="confirmation.php"  method="post">
-	Select the colour for the <?php echo $_POST[selqty] ?> widgets you are ordering
+      <?php $cookieName ='qtyNumber';
+      setcookie($cookieName, $_POST['selqty']);
+      ?>
+      <form action="confirmationcookies.php"  method="post">
+       	Select the colour for the <?php echo $_POST['selqty'] ?> widgets you are ordering
    	<select name="selcolour">
    	  <option>white</option>
 	  <option>red</option>
