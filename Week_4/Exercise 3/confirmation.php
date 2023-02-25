793becff5b240da20318cc902d754c4eec17e032
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-02-25 23:17:18 +0000

    Update confirmation.php

--- Week_4/Exercise 3/confirmation.php
+++ Week_4/Exercise 3/confirmation.php
@@ -1,4 +1,21 @@
 <?php
-   echo "<h2> Your order qty is $_POST[selqty] </h2></br>";
-   echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
+session_start();
+
+$qty = $_SESSION['selqty'];
+$price = $_POST['txtprice'];
+$color = $_SESSION['selcolour'];
+
+$total = $price * $qty;
 ?>
+
+<html>
+<head>
+	<title>Confirmation Page</title>
+</head>
+<body>
+	<p>Thank you for your purchase!</p>
+	<p>Qty: <?php echo $qty; ?></p>
+	<p>Colour: <?php echo $color; ?></p>
+	<p>Total Price: <?php echo $total; ?></p>
+</body>
+</html>
