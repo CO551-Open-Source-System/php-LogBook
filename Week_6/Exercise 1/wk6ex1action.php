1547a4c85b5a59017a96c1e4f70cb3034a9a9469
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-16 23:33:52 +0000

    Update wk6ex1action.php

--- Week_6/Exercise 1/wk6ex1action.php
+++ Week_6/Exercise 1/wk6ex1action.php
@@ -1,39 +1,50 @@
-<?php
-    // Database connection file
-    include 'Dbconnection.php';
+ <!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css"/>
+        <title>PHP log book Sudath Nawagamuwage</title>	
+    </head>
+    <body>
+        <div class="page-container">
+            <header>
+                <h1>My PHP Log book page</h1>
+            </header>
+            <div class="content-wrap">
+                <div class="container">
+                    <?php
+                         // Database connection file
+                        include 'Dbconnection.php';
 
-    //Insert the value to database
-    $sql = "INSERT INTO test (name,email,phone_number)";
-    $sql = $sql . " values ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]')";
-   
-//    // Connect to server and select database
-//     $mysqli = new mysqli("localhost","sudath","test123","db1_ex5");
-
-//     // Check connection
-//     if ($conn->connect_error) {
-//         die("Connection failed: " . $conn->connect_error);
-//     }
-
-   
-	//Execute the SQL statement to insert the data into the 'test' table
-    if ($mysqli->query($sql) === TRUE) {
-        echo "New record created successfully <br/><br/>";
-      } else {
-        echo "Error: " . $sql . "<br>" . $mysqli -> connect_error;
-      }
-	
-	
-	// Define the SQL statement for selecting all data from the 'test' table 
-	$sql = "SELECT * from test";
-    // $result = mysqli_query($conn, $sql);
-	
-    // Execute sql statement
-	$result = $mysqli->query($sql);
-	while ($row = mysqli_fetch_assoc($result))
-	{
-		echo "$row[name]  $row[email]  $row[phone_number] <br/>";
-	}
-	
-    // Close the database connection
-    // mysqli_close($conn);
-?>
+                        //Insert the value to database
+                        $sql = "INSERT INTO test (name,email,phone_number)";
+                        $sql = $sql . " values ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]')";
+                    
+                        //Execute the SQL statement to insert the data into the 'test' table
+                        if ($mysqli->query($sql) === TRUE) {
+                            echo "New record created successfully <br/><br/>";
+                        } else {
+                            echo "Error: " . $sql . "<br>" . $mysqli -> connect_error;
+                        }	
+                        // Define the SQL statement for selecting all data from the 'test' table 
+                        $sql = "SELECT * from test";
+                        // $result = mysqli_query($conn, $sql);
+                        
+                        // Execute sql statement
+                        $result = $mysqli->query($sql);
+                        while ($row = mysqli_fetch_assoc($result))
+                        {
+                            echo "$row[name]  $row[email]  $row[phone_number] <br/>";
+                        }                    
+                    ?>
+                </div>
+            </div>
+            <!-- footer -->
+            <footer class="footer">
+            <p>&copy; 2023 My Website. All rights reserved.</p>
+            </footer>
+        </div>
+    </body>
+</html>
