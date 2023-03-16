d85ecfed5d5087ae601a8bce5ac8c4cabe03e302
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-16 23:12:03 +0000

    Create wk6ex2delete.php

---
+++ Week_6/7test/wk6ex2delete.php
@@ -0,0 +1,45 @@
+<!-- Sudath Nawagamuwage
+	 CO551-Open Source System
+	 LoogBook 6-->
+<?php	
+    // Database connection file
+    include 'Dbconnection.php';	  
+	
+    // Check if the form was submitted
+    if(isset($_POST['btnupdate'])){
+        // Update query
+        $id = $_GET['id'];
+        $name = $_POST['txtname'];
+        $email = $_POST['txtemail'];
+        $phone_number = $_POST['txttelno'];
+        $sql = "UPDATE test SET name='$name', email='$email', phone_number='$phone_number' WHERE ID='$id'";
+        $result = $mysqli->query($sql);
+        if($result){
+            echo "Record updated successfully!";
+        }
+        // Redirect back to the list page
+        header('location: wk6ex2.php?msg=Record updated successfully');
+        exit;
+    }
+    
+    //Update queary
+    $sql = "UPDATE test SET 
+            name = '{$_POST['txtname']}', 
+            email = '{$_POST['txtemail']}', 
+            phone_number = '{$_POST['txttelno']}'
+        WHERE ID = '{$_GET['id']}'";
+    
+    // Check if the delete button was clicked
+    if(isset($_POST['btndelete'])){
+        $id = $_GET['id'];
+        $sql = "DELETE FROM test WHERE ID = '$id'";
+        $result = $mysqli->query($sql);
+        if($result){
+            echo "Record deleted successfully!";
+        }
+        // Redirect back to the list page
+        header('location: wk6ex2.php?msg=Record deleted successfully!');
+        exit;
+    }
+   
+?>
