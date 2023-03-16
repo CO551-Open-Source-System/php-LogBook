8da7dd3090b1375f82960c6169571987ebeeec2b
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-16 23:41:49 +0000

    Update wk6ex2.php

--- Week_6/Exercise 2/wk6ex2.php
+++ Week_6/Exercise 2/wk6ex2.php
@@ -1,21 +1,41 @@
-<?php	
-
-    // Database connection file
-    include 'Dbconnection.php';
-
-	//Selecting data from the test teble
-	$sql = "SELECT * FROM test";
-    // Execute sql statement
-	$result = $mysqli->query($sql);
-?>
+<!-- Sudath Nawagamuwage CO551-Open Source System  LoogBook 6-->
+<!DOCTYPE html>
 <html>
-<body>
-<!-- Print the result of queary  -->
-<?php
-while ($row = mysqli_fetch_assoc($result))
-{
-      echo "<a href=\"wk6ex2action.php?id=$row[ID]\">$row[name] $row[ID]</a></br>";  	
-}
-?>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css"/>
+		<title>PHP log book Sudath Nawagamuwage</title>	
+	</head>
+	<body>
+		<div class="page-container">
+			<header>
+				<h1>My PHP Log book page</h1>
+			</header>
+			<div class="content-wrap">
+				<div class="container">
+                    <?php	
+                        // Database connection file
+                        include 'Dbconnection.php';
+
+	                    //Selecting data from the test teble
+	                    $sql = "SELECT * FROM test";
+                        // Execute sql statement
+	                    $result = $mysqli->query($sql);
+                    ?>
+                    <!-- Print the result of queary  -->
+                    <?php
+                    while ($row = mysqli_fetch_assoc($result))
+                    {
+                        echo "<a href=\"wk6ex2action.php?id=$row[ID]\">$row[name] $row[ID]</a></br>";  	
+                    }
+                    ?>
+                </div>
+			</div>
+			<!-- footer -->
+			<footer class="footer">
+				<p>&copy; 2023 My Website. All rights reserved.</p>
+			</footer>
+		</div>
+	</body>
 </html>
