7349173c86d78c316557324afcb37c16b025fcdc
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-16 23:22:49 +0000

    Update wk6ex2.php

--- Week_6/Exercise 3/wk6ex2.php
+++ Week_6/Exercise 3/wk6ex2.php
@@ -1,6 +1,4 @@
-<!-- Sudath Nawagamuwage
-	 CO551-Open Source System
-	 LoogBook 6-->
+<!-- Sudath Nawagamuwage CO551-Open Source System  LoogBook 6-->
      <!DOCTYPE html>
 <html>
 	<head>
@@ -16,30 +14,27 @@
 			</header>
 			<div class="content-wrap">
 				<div class="container">
-<?php	
+					<?php	
+					// Database connection file
+					include 'Dbconnection.php';
 
-    // Database connection file
-    include 'Dbconnection.php';
+					//Selecting data from the test teble
+					$sql = "SELECT * FROM test";
+					// Execute sql statement
+					$result = $mysqli->query($sql);
+					?>
+					<!-- Print the result of queary  -->
+					<?php
+					if (isset($_GET["msg"])){
+						echo "<p>{$_GET["msg"]}</p>";
+					}
 
-	//Selecting data from the test teble
-	$sql = "SELECT * FROM test";
-    // Execute sql statement
-	$result = $mysqli->query($sql);
-?>
-<html>
-<body>
-<!-- Print the result of queary  -->
-<?php
-if (isset($_GET["msg"])){
-    echo "<p>{$_GET["msg"]}</p>";
-}
-
-while ($row = mysqli_fetch_assoc($result))
-{
-      echo "<a href=\"wk6ex2action.php?id=$row[ID]\">$row[name] $row[ID]</a></br>";  	
-}
-?>
-</div>
+					while ($row = mysqli_fetch_assoc($result))
+					{
+						echo "<a href=\"wk6ex2action.php?id=$row[ID]\">$row[name] $row[ID]</a></br>";  	
+					}
+					?>
+				</div>
 			</div>
 			<!-- footer -->
 			<footer class="footer">
