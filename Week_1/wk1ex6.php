d9a71103abdcc1d088a5a03cb2a8548f8b21dde6
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 10:11:13 +0000

    Update wk1ex6.php

--- Week_1/wk1ex6.php
+++ Week_1/wk1ex6.php
@@ -1,14 +1,20 @@
-<html>
-<body>
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
 <?php
-	$firstname = "Richard";
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding -->
+<?php
+	$firstname = "Sudath";
    // $space = " ";
-	$lastname = "Mather";
+	$lastname = "Nawagamuwage";
 	//$name = $firstname .$space. $lastname;
 	$fullname =$firstname." ".$lastname;
-	echo "$fullname<br/>";
-	echo "$firstname $lastname";
+	echo "My Fullname is :" .$fullname."</br></br>";
+	echo "My Fullname is :" .$firstname .$lastname;
 	//echo $name;
 ?>
-</body>
-</html>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>
