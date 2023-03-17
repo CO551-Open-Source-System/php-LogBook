ab39d633bd22949381e433cd3a476719ec2fe369
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 10:14:54 +0000

    Update header.php

--- Week_1/header.php
+++ Week_1/header.php
@@ -11,7 +11,7 @@
     <body>
         <div class="page-container">
             <header>
-                <h1><h1>My PHP Logbook page EX:1</h1></h1>
+                <h1><h1>My PHP Logbook Page EX:1</h1></h1>
             </header>
             <div class="content-wrap">
                 <div class="container">
