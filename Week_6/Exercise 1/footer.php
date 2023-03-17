e6ed4b3e8ef04c1376f9b89eb71869b05fb44d79
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 00:05:56 +0000

    Create footer.php

---
+++ Week_6/Exercise 1/footer.php
@@ -0,0 +1,9 @@
+</div>
+            </div>
+            <!-- footer -->
+            <footer class="footer">
+            <p>&copy; 2023 My Website. All rights reserved.</p>
+            </footer>
+        </div>
+    </body>
+</html>
