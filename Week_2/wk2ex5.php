ee38dc5b3fef84f646db3bd902a2d5c632939d63
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 11:05:49 +0000

    Update wk2ex5.php

--- Week_2/wk2ex5.php
+++ Week_2/wk2ex5.php
@@ -1,3 +1,9 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print massage -->
 <?php
 if (strpos($_SERVER["HTTP_USER_AGENT"], "MSIE") != false)
 {
@@ -8,3 +14,7 @@ else
    echo "Why are you not using Internet Explorer ?<br/>";
 }
 ?> 
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>
