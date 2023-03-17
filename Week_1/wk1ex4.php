4f0984226f9322ae307f2ee765b4690dc87893cc
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 10:11:16 +0000

    Update wk1ex4.php

--- Week_1/wk1ex4.php
+++ Week_1/wk1ex4.php
@@ -1,7 +1,16 @@
-<html>
-    <body>
-        <?php
-            echo gmdate("z");
-        ?>
-    </body>
-</html>
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding -->
+<?php
+    // zero day of the year
+    echo "Gmdate :". gmdate ("z")."</br>";
+    // outputs: 17 March 2023
+    echo "Date :". date ("j F Y");
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>                
