1cc842150099af1f682e92647e6d59aa9295d0c1
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 23:49:47 +0000

    Update selectcolourcookies.php

--- Week_4/Exercise 1/selectcolourcookies.php
+++ Week_4/Exercise 1/selectcolourcookies.php
@@ -1,27 +1,24 @@
-<!-- <html>
-  <head><title>Select colour page</title></head>
-    <body> -->
-    <?php
-    // Include header.php
-    include 'header.php';
+<?php
+// Include header.php
+include 'header.php';
 ?>
-<!-- Php coding to print moudle details using foreach loops  -->
-      <?php $cookieName ='qtyNumber';
-      setcookie($cookieName, $_POST['selqty']);
-      ?>
-      <form action="confirmationcookies.php"  method="post">
-       	Select the colour for the <?php echo $_POST['selqty'] ?> widgets you are ordering
-   	<select name="selcolour">
-   	  <option>white</option>
-	  <option>red</option>
-	  <option>yellow</option>
-	  <option>green</option>
-	  <option>blue</option>
-        </select>
-        <br/><br/>	
-        <input type="submit" value="Buy"/>
-      </form>
-      <?php
-    // Include footer.php
-    include 'footer.php';
+<!-- Php coding to -->
+<?php $cookieName ='qtyNumber';    
+setcookie($cookieName, $_POST['selqty']);
+?>
+<form action="confirmationcookies.php"  method="post">
+Select the colour for the <?php echo $_POST['selqty'] ?> widgets you are ordering
+<select name="selcolour">
+<option>white</option>
+<option>red</option>
+<option>yellow</option>
+<option>green</option>
+<option>blue</option>
+</select>
+<br/><br/>	
+<input type="submit" value="Buy"/>
+</form>
+<?php
+// Include footer.php
+include 'footer.php';
 ?>
