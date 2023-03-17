1ddcd4f6535dee6697f64eb07458086ff47a8688
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 11:05:53 +0000

    Update wk2ex7.php

--- Week_2/wk2ex7.php
+++ Week_2/wk2ex7.php
@@ -1,6 +1,18 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
 <?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print for loop -->
+<?php
+  echo "Number of count :</br></br>";
+
   for($count = 0; $count <= 10;$count++);
   {
     echo "I must behave in class  $count <br/>";
   }
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
 ?>
