2c7d98ff3642a68d4e627e3b2ba0bf4dcbe2c662
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 10:14:58 +0000

    Update wk2ex1.php

--- Week_2/wk2ex1.php
+++ Week_2/wk2ex1.php
@@ -1,11 +1,16 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to caculation the wages -->
 <?php 
     $hourlyrate	= 5.75;
 	$hoursperweek = 40;
 	$gross = $hourlyrate * $hoursperweek;
 ?>	
-<html>
-<head></head>
-<body>
-	<p> My gross wage is <?php print("$gross"); ?>
-</body>
-</html>
+<p> My gross wage is : <?php print("£ $gross"); ?>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>
