e91fd946886a265cf2facf4a27e180e620b2b65f
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 11:05:45 +0000

    Update wk2ex3.php

--- Week_2/wk2ex3.php
+++ Week_2/wk2ex3.php
@@ -1,3 +1,9 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print my pass grde -->
 <?php 
 	define("PASS_GRADE",40);
 	echo "You need " . PASS_GRADE . " % or more to pass. <br/>";
@@ -5,4 +11,8 @@
 	echo "To achieve a merit you need $grade %  or more <br/>";
     $grade = PASS_GRADE + 30;
 	echo "To achieve a distinction you need $grade %  or more <br/>";
+?>	
+<?php
+    // Include footer.php
+    include 'footer.php';
 ?>	
