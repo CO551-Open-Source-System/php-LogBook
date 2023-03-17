ec0f834ed405b2f716842f30ab0651cdaa895c89
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 12:21:03 +0000

    Update confirmationcookies.php

--- Week_4/Exercise 1/confirmationcookies.php
+++ Week_4/Exercise 1/confirmationcookies.php
@@ -1,5 +1,16 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print moudle details using foreach loops  -->
 <?php
    // echo "<h2> Your order qty is $_POST[selqty] </h2></br>";
    echo "<h2> Your order qty is". $_COOKIE['qtyNumber'] ."</h2></br>";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
 ?>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>
+
