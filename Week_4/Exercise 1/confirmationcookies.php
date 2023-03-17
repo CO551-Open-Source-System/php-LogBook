78141bb137696d4ae0c30d8e591c51c95d6ed35b
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 15:27:23 +0000

    Update confirmationcookies.php

--- Week_4/Exercise 1/confirmationcookies.php
+++ Week_4/Exercise 1/confirmationcookies.php
@@ -3,11 +3,11 @@
     // Include header.php
     include 'header.php';
 ?>
-<!-- Php coding to print moudle details using foreach loops  -->
+<!-- Php coding to print the quntity using cookie-->
 <?php
    // echo "<h2> Your order qty is $_POST[selqty] </h2></br>";
-   echo "<h2> Your order qty is". $_COOKIE['qtyNumber'] ."</h2></br>";
-   echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
+   echo "<h2> Your order qty is : ". $_COOKIE['qtyNumber'] ."</h2></br>";
+   echo "<h2> and the selected colour is : $_POST[selcolour].</h2>"
 ?>
 <?php
     // Include footer.php
