9e8ac2bd7ba58121e80d7455badcabf86cf23470
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 11:25:38 +0000

    Update wk2ex10.php

--- Week_2/wk2ex10.php
+++ Week_2/wk2ex10.php
@@ -1,3 +1,9 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print moudle marks using foreach loops  -->
 <?php
   $mymarks["year 1"] = 55;
   $mymarks["year 2"] = 65;
@@ -9,6 +15,11 @@
     echo "for  $index  my grade was $mymarks[$index] <br/>";
   }
   echo "<br/> My best year was Year 3 when I averaged ". $mymarks["year 3"];
+?>
+
+<?php
+    // Include footer.php
+    include 'footer.php';
 ?>
 
 
