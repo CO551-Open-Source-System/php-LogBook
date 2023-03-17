fe82974b4995afe2f12caefa05d4aee0a00d328b
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 23:49:52 +0000

    Update selectcoloursession.php

--- Week_4/Exercise 1/selectcoloursession.php
+++ Week_4/Exercise 1/selectcoloursession.php
@@ -3,24 +3,23 @@
     // Include header.php
     include 'header.php';
 ?>
-      <?php session_start();?>
-        <form action="confirmationsession.php"  method="post">
-       	Select the colour for the 
-        <?php $_SESSION['qtyNumber'] = $_POST['selquty'] 
-        = $_POST['selqty'] 
-        ?>
-         widgets you are ordering
-   	<select name="selcolour">
-   	  <option>white</option>
-	  <option>red</option>
-	  <option>yellow</option>
-	  <option>green</option>
-	  <option>blue</option>
-        </select>
-        <br/><br/>	
-        <input type="submit" value="Buy"/>
-      </form>
-      <?php
-    // Include footer.php
-    include 'footer.php';
+<?php session_start();?>
+<form action="confirmationsession.php"  method="post">
+Select the colour for the 
+<?php $_SESSION['qtyNumber'] = $_POST['selquty']  = $_POST['selqty'] 
+?>
+widgets you are ordering
+<select name="selcolour">
+<option>white</option>
+<option>red</option>
+<option>yellow</option>
+<option>green</option>
+<option>blue</option>
+</select>
+<br/><br/>	
+<input type="submit" value="Buy"/>
+</form>
+<?php
+  // Include footer.php
+  include 'footer.php';
 ?>
