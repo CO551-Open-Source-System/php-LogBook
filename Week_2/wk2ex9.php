82f77365486d8f79dcad8d5abb0e801aa18fe1f6
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 11:25:47 +0000

    Update wk2ex9.php

--- Week_2/wk2ex9.php
+++ Week_2/wk2ex9.php
@@ -1,3 +1,9 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print moudle details using foreach loops  -->
 <?php
   $topmodules[0] = "Internet Systems Development";
   $topmodules[5] = "Programming 1";
@@ -5,10 +11,21 @@
   $topmodules[30] = "OOAD";
   $topmodules[40] = "Software Engineering";
 
+  echo 'My Module details :'.'</br></br>';
+  echo '<table border=1>';
+  echo '<tr><th>Index</th><th>Subject</th></tr>';
+
   foreach ($topmodules as $index=>$value) {
-    
-    echo "Index is $index and value is $value <br/>";
+    echo '<tr>';
+    echo '<td>'.$index.'value is'.'</td>';
+    echo '<td>'.$value.'</td></tr>';    
+    // echo "Index is $index and value is $value <br/>";
   }
+  echo '</table>';
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
 ?>
 
 
