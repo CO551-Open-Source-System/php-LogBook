77c6a88edd36d1993d85272984c5c6818126f34f
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 10:11:07 +0000

    Update wk1ex3.php

--- Week_1/wk1ex3.php
+++ Week_1/wk1ex3.php
@@ -1,9 +1,16 @@
-<html>
-    <body>
-        <?php
-            echo "Blimmy it worked!";
-        ?>
-    </body>
-</html>
-
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding -->
+<?php
+    echo "Blimmy it worked!";
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>
+                
+    
     
