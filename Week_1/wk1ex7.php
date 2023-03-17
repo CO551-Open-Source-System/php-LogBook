8f1107491d0c8767c4a74251c17183720b2969f3
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 10:11:18 +0000

    Update wk1ex7.php

--- Week_1/wk1ex7.php
+++ Week_1/wk1ex7.php
@@ -1,21 +1,23 @@
-<html>
-    <header>
-        <title>My Php Lob Book website</title>
-    </header>
-    <body>
-        <center>
-            <?php
-                $hourlyrate = 5.75;
-                $hoursperweek = 40;
-                $taxtrate =.22;
-                $gross = $hourlyrate * $hoursperweek;
-                echo $gross, "<br/>";
-                $taxtrate = $gross*$taxtrate;
-                echo $taxtrate, "<br/>";
-                $netwage =$gross-$taxtrate;
-                echo $netwage;
-            ?>
-        </center>
-    </body>
-</html>
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding -->
+<?php
+    $hourlyrate = 5.75;
+    $hoursperweek = 40;
+    $taxtrate =.22;
+    $gross = $hourlyrate * $hoursperweek;
+    echo "Hour lyrate Is = £ ".$gross, "<br/>";
+    $taxtrate = $gross*$taxtrate;
+    echo "Tax Rate Is = £ ".$taxtrate, "<br/>";
+    $netwage =$gross-$taxtrate;
+    echo"Net Wage Is = £ " . $netwage;
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>
+        
 
