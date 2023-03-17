59ca5f914f76115e1559c0166db5591791963eae
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 11:05:42 +0000

    Update wk2ex2.php

--- Week_2/wk2ex2.php
+++ Week_2/wk2ex2.php
@@ -1,11 +1,21 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print my age different way -->
 <?php 
 	$myage = "None of your business <br/>";
 	print($myage);
 	
 	$myage  = 21;
-    print($myage. "<br/>");
+    print"Mt Age is :".($myage. "<br/>");
     
 	$myage = 21.75;
-	print($myage);
+	print"Mt Age is :".($myage);
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
 ?>	
 
