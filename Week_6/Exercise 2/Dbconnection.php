922e7bc4e5907a64c43549add6dfcc791c68e6f9
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 09:03:57 +0000

    Update Dbconnection.php

--- Week_6/Exercise 2/Dbconnection.php
+++ Week_6/Exercise 2/Dbconnection.php
@@ -1,5 +1,8 @@
 
 <?php
+// Connect to server and select database at the intweb MyPhpAdmin
+// $mysqli = new mysqli("localhost","21428786","mysqluser","db1_21428786");   
+
 // Connect to server and select database
     $mysqli = new mysqli("localhost","sudath","test123","db1_ex5");
 
@@ -8,4 +11,5 @@
     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
     exit();
   }
+?>
 ?>
