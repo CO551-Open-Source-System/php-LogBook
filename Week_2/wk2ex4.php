acb00410157867bd00f178fdfbec67e661a1328f
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 11:05:46 +0000

    Update wk2ex4.php

--- Week_2/wk2ex4.php
+++ Week_2/wk2ex4.php
@@ -1,5 +1,9 @@
-<! - - Speed camera advise script - ->
-
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print Speed camera advise script -->
 <?php
   $points = 10;
  // Output appropriate message depending on points awarded.
@@ -16,3 +20,7 @@ else
    echo "There is no need to worry about the speed limit.<br/>";
 }
 ?> 
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>
