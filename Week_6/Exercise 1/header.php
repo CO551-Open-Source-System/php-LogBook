40ca0587849ff01b577cfa76493ebdfb399f2a86
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 00:05:55 +0000

    Create header.php

---
+++ Week_6/Exercise 1/header.php
@@ -0,0 +1,15 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css"/>
+        <title>PHP log book Sudath Nawagamuwage</title>	
+    </head>
+    <body>
+        <div class="page-container">
+            <header>
+                <h1>My PHP Log book page</h1>
+            </header>
+            <div class="content-wrap">
+                <div class="container">
