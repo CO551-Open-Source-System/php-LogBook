2bad827b4eb856f3d7b34a4ea6e9a2da20011e44
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 09:12:46 +0000

    Update wk6ex5.php

--- Week_6/Exercise 5/wk6ex5.php
+++ Week_6/Exercise 5/wk6ex5.php
@@ -1,6 +1,32 @@
-<?php
-	include("myfunctions.inc");
-	html_header("My first function demo");
-	html_h1("These functions are going to save me lots of time");
-	html_footer();
-?>
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css"/>
+    <title>PHP log book Sudath Nawagamuwage</title>	
+</head>
+<body>
+	<div class="page-container">
+		<header>
+			<h1>My PHP Log book page</h1>
+		</header>
+		<div class="content-wrap">
+			<div class="container">
+				<!-- php coding -->
+				<?php
+					include("myfunctions.inc");
+					html_header("My first function demo");
+					html_h1("These functions are going to save me lots of time");
+					html_footer();
+				?>
+				</div>
+		</div>
+		<!-- footer -->
+		<footer class="footer">
+        	<p>&copy; 2023 My Website. All rights reserved.</p>
+		</footer>
+	</div>
+</body>
+</html>
