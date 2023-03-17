c9d1818338df7544df556881ead9d97e8c60e491
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 11:05:51 +0000

    Update wk2ex6.php

--- Week_2/wk2ex6.php
+++ Week_2/wk2ex6.php
@@ -1,3 +1,9 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print marks array list -->
 <?php
   $marks[0] = 65;
   $marks[1] = 55;
@@ -5,7 +11,9 @@
   $marks[3] = 55;
   $marks[4] = 76;
   $marks[5] = 55;
-    
+
+  echo "Index list :</br></br>";
+  
   echo " Index 0 = $marks[0] <br/>";
   echo " Index 1 = $marks[1] <br/>";
   echo " Index 2 = $marks[2] <br/>";
@@ -13,3 +21,8 @@
   echo " Index 4 = $marks[4] <br/>";
   echo " Index 5 = $marks[5] <br/>"; 
 ?>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>	
+
