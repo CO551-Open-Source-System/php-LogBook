f3f612b949a0dec868f57f8fa31303c5a46a0540
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 10:11:11 +0000

    Update wk1ex5.php

--- Week_1/wk1ex5.php
+++ Week_1/wk1ex5.php
@@ -1,8 +1,15 @@
-<html>
-    <body>
-        <?php
-	       $myfavouritemodule  = "Web developmet";
-           echo $myfavouritemodule;
-        ?>
-    </body>
-</html>
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding -->
+<?php
+    $myfavouritemodule  = "Web developmet";
+    echo $myfavouritemodule;
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>                
+   
