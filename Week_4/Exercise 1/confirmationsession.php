26e2d98331c637a120f72940eb7ab405c753c998
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 15:27:28 +0000

    Update confirmationsession.php

--- Week_4/Exercise 1/confirmationsession.php
+++ Week_4/Exercise 1/confirmationsession.php
@@ -1,5 +1,15 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print moudle details using foreach loops  -->
 <?php
    session_start();
    echo "<h2> Your order qty is". $_SESSION['qtyNumber']." </h2></br>";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
 ?>
