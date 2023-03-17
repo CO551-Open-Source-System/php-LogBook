831c6e53efad81a8aa36e4d78fa76d0a769108d3
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 15:27:25 +0000

    Update confirmationhidden.php

--- Week_4/Exercise 1/confirmationhidden.php
+++ Week_4/Exercise 1/confirmationhidden.php
@@ -1,4 +1,14 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
 <?php
-   echo "<h2> Your order qty is $_POST[hdselqty] </h2></br>";
-   echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
+    // Include header.php
+    include 'header.php';
+?>
+<!-- Php coding to print the quntitiy using hidden function -->
+<?php
+   echo "<h2> Your order qty is : $_POST[hdselqty] </h2></br>";
+   echo "<h2> and the selected colour is : $_POST[selcolour].</h2>"
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
 ?>
