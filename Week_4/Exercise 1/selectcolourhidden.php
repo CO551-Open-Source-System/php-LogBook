bd6ba54927c9906a87e8ef2f7ad7a352faf7c9bf
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-17 23:49:49 +0000

    Update selectcolourhidden.php

--- Week_4/Exercise 1/selectcolourhidden.php
+++ Week_4/Exercise 1/selectcolourhidden.php
@@ -3,21 +3,21 @@
     // Include header.php
     include 'header.php';
 ?>
-<!-- Php coding to print moudle details using foreach loops  -->
-      <form action="confirmationhidden.php"  method="post">
-	Select the colour for the <input type="hidden" name="hdselqty"
-   value="<?= $_POST['selqty'] ?>"/> widgets you are ordering
-   	<select name="selcolour">
+<!-- Php coding to print -->
+<form action="confirmationhidden.php"  method="post">
+Select the colour for the <input type="hidden" name="hdselqty"
+value="<?= $_POST['selqty'] ?>"/> widgets you are ordering
+<select name="selcolour">
    	  <option>white</option>
 	  <option>red</option>
 	  <option>yellow</option>
 	  <option>green</option>
 	  <option>blue</option>
         </select>
-        <br/><br/>	
-        <input type="submit" value="Buy"/>
-      </form>
+<br/><br/>	
+<input type="submit" value="Buy"/>
+</form>
 <?php
-    // Include footer.php
-    include 'footer.php';
+// Include footer.php
+include 'footer.php';
 ?>
