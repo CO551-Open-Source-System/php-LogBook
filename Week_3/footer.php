1805b6a97865a69dee72e89b3e6573a415932f7a
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-18 00:04:58 +0000

    Create footer.php

---
+++ Week_3/footer.php
@@ -0,0 +1,11 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<!-- footer file for my page -->
+            </div>
+			</div>
+		<!-- footer -->
+		<footer class="footer">
+        	<p>&copy; 2023 My Website. All rights reserved.</p>
+		</footer>
+	</div>
+</body>
+</html>
