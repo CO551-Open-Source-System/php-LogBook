0f6ea5470a4c3494efc184398b874e280300492e
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-18 00:04:54 +0000

    Update confirmationsession.php

--- Week_4/Exercise 1/confirmationsession.php
+++ Week_4/Exercise 1/confirmationsession.php
@@ -6,8 +6,8 @@
 <!-- Php coding to print moudle details using foreach loops  -->
 <?php
    session_start();
-   echo "<h2> Your order qty is". $_SESSION['qtyNumber']." </h2></br>";
-   echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
+   echo "<h2> Your order qty is :". $_SESSION['qtyNumber']." </h2></br>";
+   echo "<h2> and the selected colour is : $_POST[selcolour].</h2>"
 ?>
 <?php
     // Include footer.php
