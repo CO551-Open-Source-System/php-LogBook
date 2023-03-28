13c39748a5538c3643e469392663dfcb28f1e4ca
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-28 12:26:25 +0100

    Update getjpg.php

--- Week_7/Exercise 4/getjpg.php
+++ Week_7/Exercise 4/getjpg.php
@@ -3,12 +3,12 @@
   header("Content-type: image/jpeg");
 
   // Database connection file
-  //include 'Dbconnection.php';
   $conn = mysqli_connect("localhost","sudath","test123","db2_monster");
 
-
+  // Define the SQL statement for selecting id to print the image sound
   $sql = "SELECT image FROM monster WHERE id='" . $_GET['id'] ."';";
 	
+   // Execute sql statement
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_array($result);
   
