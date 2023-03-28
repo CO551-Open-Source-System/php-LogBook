4c50f4d89eb319941656e8e6d56b436924b50f3d
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-28 13:59:38 +0100

    Update savemonster.php

--- Week_7/Exercise 2/savemonster.php
+++ Week_7/Exercise 2/savemonster.php
@@ -1,27 +1,27 @@
 <?php
-// Database connection file
-include 'Dbconnection.php';
 
-// $db = mysqli_connect("localhost", "mysqlusername", "mysqlpassword", "mysqldatabase");
+  // Database connection file
+  include 'Dbconnection.php';
 
-// Obtain the file sent to the server within the response.
-$image = $_FILES['monsterimage']['tmp_name']; 
-$audio = $_FILES['monsteraudio']['tmp_name'];
+  // Obtain the file sent to the server within the response.
+  $image = $_FILES['monsterimage']['tmp_name']; 
+  $audio = $_FILES['monsteraudio']['tmp_name'];
 
   // Get the file binary data
   $imagedata = addslashes(fread(fopen($image, "r"), filesize($image)));
   $audiodata = addslashes(fread(fopen($audio, "r"), filesize($audio)));
-   
+  
+  //Insert the value to database  
   $sql = "INSERT INTO monster";
   $sql .= "(name, image, audio) ";
   $sql .= "VALUES ('$_POST[txtname]', '$imagedata','$audiodata');";
 
-//   $result = $mysqli->query($sql);
+  //Execute the SQL statement 
   $result = mysqli_query($db, $sql);
   if($result){
-    echo "Record updated successfully!";
-}
-// Redirect back to the list page
-header('location: monsterform.html?msg=Record updated successfully');
-mysqli_close($db);
+  echo "Record updated successfully!";
+  }
+  // Redirect back to the list page
+  header('location: monsterform.html?msg=Record updated successfully');
+  mysqli_close($db);
 ?>
