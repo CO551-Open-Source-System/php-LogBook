41734b065b04ce838c6c707cdbed9cc0d4485891
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-28 23:13:25 +0100

    update screenshots and folder

--- Week_6/Exercise 2/wk6ex2.php
+++ Week_6/Exercise 2/wk6ex2.php
@@ -17,14 +17,15 @@
                     <?php	
                         // Database connection file
                         include 'Dbconnection.php';
-
+					?>
+					<?php
 	                    //Selecting data from the test teble
 	                    $sql = "SELECT * FROM test";
                         // Execute sql statement
 	                    $result = $mysqli->query($sql);
-                    ?>
-                    <!-- Print the result of queary  -->
-                    <?php
+                    
+                    // <!-- Print the result of queary  -->
+                    
                     while ($row = mysqli_fetch_assoc($result))
                     {
                         echo "<a href=\"wk6ex2action.php?id=$row[ID]\">$row[name] $row[ID]</a></br>";  	
