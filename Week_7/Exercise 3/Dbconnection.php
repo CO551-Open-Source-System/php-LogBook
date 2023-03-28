d7cf27f7fe5ea10bf16adb501de1b03c944311ca
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-28 12:26:42 +0100

    Update Dbconnection.php

--- Week_7/Exercise 3/Dbconnection.php
+++ Week_7/Exercise 3/Dbconnection.php
@@ -1,9 +1,7 @@
 
 <?php
     // Connect to server and select database
-    // $db = new mysqli("localhost","sudath","test123","db2_monster");
     $conn = mysqli_connect("localhost","sudath","test123","db2_monster");
-
     
     // Check connection
     if ($conn -> connect_errno) {
