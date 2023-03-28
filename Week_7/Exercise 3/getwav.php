73f27bda9d8b8b63242baad40c29284bebc81419
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-28 12:26:39 +0100

    Update getwav.php

--- Week_7/Exercise 3/getwav.php
+++ Week_7/Exercise 3/getwav.php
@@ -2,13 +2,13 @@
 
   header("Content-type: audio/wav");
 
-  // Database connection file
-   //include 'Dbconnection.php';
-  $conn = mysqli_connect("localhost","sudath","test123","db2_monster");
- 
-
+  // Database connection file   
+  $conn = mysqli_connect("localhost","sudath","test123","db2_monster"); 
+  
+  // Define the SQL statement for selecting id to print the audio sound
   $sql = "SELECT audio FROM monster WHERE id='" . $_GET['id'] ."';";
   
+  // Execute sql statement
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_array($result);
 
