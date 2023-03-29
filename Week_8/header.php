700cb87ac870d6349588e79a7450d3250f0daacc
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-29 10:04:22 +0100

    created php file for ex8 .1

---
+++ Week_8/header.php
@@ -0,0 +1,17 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 8-->
+<!-- heade file for my page -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css"/>
+        <title>PHP log book Sudath Nawagamuwage</title>	
+    </head>
+    <body>
+        <div class="page-container">
+            <header>
+                <h1><h1>My PHP Logbook Page</h1></h1>
+            </header>
+            <div class="content-wrap">
+                <div class="container">
---
+++ Week_8/wk9ex1.php
@@ -0,0 +1,17 @@
+<!-- Sudath Nawagamuwage CO551-Open Source System LoogBook 6-->
+<?php
+    // Include header.php
+    include 'header.php';
+?>
+<?php
+     $lottodate = date("d m Y");
+     echo "The lottery numbers for $lottodate are : ";
+     for($n=1;$n<7;$n++){
+        $number[$n] = rand(1,49);
+        echo "<br/> $number[$n]";
+     }
+?>
+<?php
+    // Include footer.php
+    include 'footer.php';
+?>
